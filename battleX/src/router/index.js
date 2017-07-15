bbe2afde1f5c0ef60c5ce89b3ba9bf9dce33bef7
System Administrator <root@MBP-de-Sabrina.gateway>
2017-07-16 05:04:34 +1000

    ajout components

--- battleX/src/router/index.js
+++ battleX/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import BattleX from '@/components/BattleX'
 
 Vue.use(Router)
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    }
+    ,
+    {
+      path: '/jeu',
+      name: 'BattleX',
+      component: app
     }
   ]
 })
